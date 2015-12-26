c8fc7cc807e6e590831ba2ea5576e4925b7cc811
Ahmad Aghazadeh <ahmad.aghazadeh.a@gmail.com>
2015-12-26 11:22:23 +0330

    Add .trivis.yml file 4

--- app/src/main/java/ahmad/aghazadeh/buildgradlecommnadline/MainActivity.java
+++ app/src/main/java/ahmad/aghazadeh/buildgradlecommnadline/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String سلام="سسس";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        سلام="طططط";
+        setTitle(سلام);
     }
 }
